5ef6da3f00dba5c72acddc9ed2db01934d3b9953
gladja <user@MacBook-Pro-user.local>
2023-08-01 23:05:58 +0300

    refactor5

--- src/js/api.js
+++ src/js/api.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
-const API_KEY = '38551028-7f205e86e4b61da8a00434006'
+const API_KEY = '38551028-7f205e86e4b61da8a00434006';
+const BASE_URL = 'https://pixabay.com/api/';
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 const getContactService = async (search, page) => {
   try {
-   return await axios.get(`?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&&per_page=40&page=${page}`)
+   return await axios.get(`${BASE_URL}?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&&per_page=40&page=${page}`)
   } catch (error) {
     // console.log(error);
     Notiflix.Notify.failure('Sorry ERROR. Please try again.');
