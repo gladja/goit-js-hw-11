c03055ca4e412c3b56627fc329495582df1484bf
gladja <gladja@MacBook-Pro-gladja.local>
2023-08-02 11:49:45 +0300

    refactor api

--- src/js/api.js
+++ src/js/api.js
@@ -9,7 +9,7 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 const getContactService = async (search, page) => {
   try {
     return await axios.get(
-      `https://pixabay.com/api/?key=38551028-7f205e86e4b61da8a00434006&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+      `https://pixabay.com/api/?key=38551028-7f205e86e4b61da8a00434006&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
     );
   } catch (error) {
     // console.log(error);
