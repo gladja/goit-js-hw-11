31e61e97ae2bb3216f087750b1a946119d64d001
gladja <gladja@MacBook-Pro-gladja.local>
2023-08-02 11:34:54 +0300

    refactor api

--- src/js/api.js
+++ src/js/api.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 
 const API_KEY = '38551028-7f205e86e4b61da8a00434006';
-const BASE_URL = 'https://pixabay.com/api/';
+// const BASE_URL = 'https://pixabay.com/api/';
 
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 const getContactService = async (search, page) => {
   try {
     return await axios.get(
-      `${BASE_URL}?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+      `?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
     );
   } catch (error) {
     // console.log(error);
